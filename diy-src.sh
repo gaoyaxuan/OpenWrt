94f4b70d969b1cf9b78aa844fb6ab30a00115cc9
23333 <qmd3dtv4bkcwa7yd02fry@duck.com>
2024-04-11 11:48:53 +0800

    update

---
+++ diy-src.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "src-git kenzok https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
