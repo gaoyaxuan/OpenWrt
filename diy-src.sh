25daf13c770e5a44dbb0c170409964036979fa61
23333 <qmd3dtv4bkcwa7yd02fry@duck.com>
2024-05-23 20:59:31 +0800

    update

--- diy-src.sh
+++ diy-src.sh
@@ -10,7 +10,7 @@ echo "src-git telephony https://git.openwrt.org/feed/telephony.git" >> ./feeds.c
 
 
 # Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
+git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
